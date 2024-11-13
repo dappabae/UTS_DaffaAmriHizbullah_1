6fad8bb6e360e0b57a75e7e1ed746357f677351d
dappabae <chuloappa@gmail.com>
2024-11-13 12:07:41 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,32 @@
+#soal no 1
+a=int(input('masukan bilangan 1:'))
+b=int(input('masukan bilangan 2:'))
+c=a+b
+d=a-b
+e=a*b
+print(f'hasil penjumlahan :{c}')
+print(f'hasil pengurangan :{d}')
+print(f'hasil pembagian :{e}')
+
+#soal no 3
+barang = [
+    {'barang':'hp','jumlah': 2,'harga1': 5000,
+     'barang':'laptop','jumlah': 1,'harga2': 10000}
+]
+barang = int(input('masukan jumlah barang:'))
+harga1 = int(input('masukan harga 1 :'))
+harga2 = int(input('masukan harga 2 :'))
+
+if(barang + harga1)and(barang + harga2):
+    hasil = 'total'
+else:
+    hasil = 'total'
+print('barang:')
+print('jumlah')
+print('harga')
+
+#soal no 4
+BMI = 50
+while BMI < 18.5:
+    print(f)
+
